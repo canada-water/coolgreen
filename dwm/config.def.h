c338b19bd1fc7a8431bd85df8cef401065db87d9
Joel Ericsson <freshnessfromthenorth@gmail.com>
2020-12-23 17:57:52 -0500

    expanded gaps

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx	    = 5;	/* gaps between windows */
+static const unsigned int gappx	    = 10;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
