af76c9dbcd6a27006dea8a8136e87bbe5da817a0
Joel Ericsson <freshnessfromthenorth@gmail.com>
2020-12-24 08:59:19 -0500

    fixed colors

--- slock/config.def.h
+++ slock/config.def.h
@@ -4,15 +4,15 @@ static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#274D4C",   /* during input */
+	[FAILED] = "#36888f",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "$cowsay "$(fortune)")";
+static const char * message = "coolgreen";
 
 /* text color */
 static const char * text_color = "#ffffff";
--- slock/config.h
+++ slock/config.h
@@ -4,9 +4,18 @@ static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#274D4C",   /* during input */
+	[FAILED] = "#36888f",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "coolgreen";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
